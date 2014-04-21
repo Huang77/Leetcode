8b2a86b2bb499f675ceeb91b858731638b06f552
Huang77 <huangxinxin07@gmail.com>
2014-04-21 08:20:18 +0800

    SingleNumber

---
+++ SingleNumber.java
@@ -0,0 +1,13 @@
+/* 一个整数数组，其中只有一个元素只出现一次，其他元素都出现两次，请找到这个出现一次的数*/
+/* 线性时间，并且不需要用额外的空间*/
+
+/* 因为A^B^A=B, 所以对数组进行一次异或就可以得到只出现一次的数 */
+
+public class SingleNumber {
+	public int findSingle (int[] A) {
+		for (int i = 0; i < A.length; i++) {
+			A[i] ^= A[i-1];
+		}
+		return A[A.length];
+	}
+}
